8fff82188a82d4a41e8575c3b5d3127723f67445
n4rd0 <58040410+n4rd0@users.noreply.github.com>
2023-02-09 20:32:14 +0100

    Tutorial 09_02_23

---
+++ main.js
@@ -0,0 +1,22 @@
+
+//Tipos variables let, local, var, global, const, constante
+
+let numeroLet = 23;
+var numeroVar = 43;
+
+
+//Salta por pantalla como alerta en el navegador 
+    //alert(numeroLet+numeroVar)
+
+//Devuelve la respuesta del navegador 
+    //let answer = prompt("Que pasa pa")
+
+//Escribir en la pagina
+
+    //document.write(numeroLet)
+
+//Operadores: + - * / % ** -- ++ 
+    //alert(--numeroLet)
+
+//Concatenar strings
+    //string1.concat(string2) o `tienes ${numeroLet} anios`
